1d451ce4fc1fb3a43b361674ee4e7d8fbcabd17b
Chamod Keshan <98629920+ChamodKeshan@users.noreply.github.com>
2022-08-21 13:09:23 +0530

    Create helper.js

---
+++ helper.js
@@ -0,0 +1,22 @@
+/* Copyright (C) 2021 Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX.
+*/
+
+function successfullMessage(msg) {
+    return "👸 *QUEEN ALEXA*:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "👸 *QUEEN ALEXA*:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "👸 *QUEEN ALEXA*:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
