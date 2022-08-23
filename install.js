ade4cad12da70fbb263f85d3eb63fef0919e8fcb
Chamod Keshan <98629920+ChamodKeshan@users.noreply.github.com>
2022-08-23 08:13:31 +0530

    Update install.js

--- install.js
+++ install.js
@@ -60,7 +60,7 @@ async function startRedDragonMdNx() {
     const RedDragonMdNx = NexusNwIncConnect({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: true,
-        browser: ['Gojo Satoru\Nexus','Safari','1.0.0'],
+        browser: ['Chrome','Safari','1.0.0'],
         auth: state
     })
 
