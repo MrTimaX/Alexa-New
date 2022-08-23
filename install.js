7d8d9834962f1c7968f7d2b5647e09e8caa518c9
Chamod Keshan <98629920+ChamodKeshan@users.noreply.github.com>
2022-08-23 08:20:39 +0530

    Update install.js

--- install.js
+++ install.js
@@ -6,6 +6,7 @@
 //════════════════════════════//
 
 require('./settings')
+const config = require('./settings')
 const { default: NexusNwIncConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
 const pino = require('pino')
